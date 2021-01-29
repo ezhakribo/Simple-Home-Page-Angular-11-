4aee09b9f3be12538324e4a87bef1a40c33f741b
ezhakribo <ezhacaw@gmail.com>
2021-01-29 10:01:33 +0700

    8 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,21 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
+import {
+  FontAwesomeModule,
+  FaIconLibrary
+} from "@fortawesome/angular-fontawesome";
+import { faSquare, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSquare as farSquare,
+  faCheckSquare as farCheckSquare
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faStackOverflow,
+  faGithub,
+  faMedium
+} from "@fortawesome/free-brands-svg-icons";
+
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { HomeComponent } from "./home/home.component";
@@ -11,10 +26,10 @@ import { ActivityComponent } from "./activity/activity.component";
 import { PeopleComponent } from "./people/people.component";
 import { ChannelComponent } from "./channel/channel.component";
 import { DocumentComponent } from "./document/document.component";
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, FontAwesomeModule],
   declarations: [
     AppComponent,
     HelloComponent,
@@ -29,4 +44,16 @@ import { FooterComponent } from './footer/footer.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(
+      faSquare,
+      faCheckSquare,
+      farSquare,
+      farCheckSquare,
+      faStackOverflow,
+      faGithub,
+      faMedium
+    );
+  }
+}
